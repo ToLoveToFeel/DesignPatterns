23da9f6888d4035c8bff5e70a3cec48a5f287ff1
i-love-linux <1137247975@qq.com>
2019-08-26 19:09:53 +0000

    中介者模式

---
+++ src/MediatorPattern/MediatorPatternDemo.java
@@ -0,0 +1,12 @@
+//18.中介者模式
+package MediatorPattern;
+
+public class MediatorPatternDemo {
+    public static void main(String[] args) {
+        User robert = new User("Robert");
+        User john = new User("John");
+
+        robert.sendMessage("Hi! John!");
+        john.sendMessage("Hello! Robert!");
+    }
+}
